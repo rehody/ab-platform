d7f862548698af1b05abd5f4346c718614c10d93
Michael <mgoronkov23@gmail.com>
2026-03-21 22:53:21 +0500

    init

---
+++ src/main/java/io/github/rehody/abplatform/AbPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.rehody.abplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbPlatformApplication.class, args);
+    }
+
+}
